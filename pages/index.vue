f0d9a1798466259eec0842c462969283c72d722e
Akin Akinbobola <akinbobola71@gmail.com>
2024-02-20 10:29:43 +0100

    Add Top Menu

--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,9 @@
-<script setup lang="ts">
+<script setup>
 
 </script>
 
 <template>
-  <h1 class="text-3xl font-bold underline">
-    Hello world!
-  </h1>
+  <div id="IndexPage" class="mt-4 max-w-[1200px] mx-auto px-2">
+    Index Page
+  </div>
 </template>
