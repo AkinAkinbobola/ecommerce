d226e0d748775c2502e6bcbe4c6fb42cd5e5c4cd
Akin Akinbobola <akinbobola71@gmail.com>
2024-02-21 20:07:44 +0100

    Add hover change image

--- pages/item/[id].vue
+++ pages/item/[id].vue
@@ -1,5 +1,5 @@
 <script setup>
-const currentImage = ref(null);
+const currentImage = ref("");
 const images = ref([
   "",
   'https://picsum.photos/id/95/800/800',
@@ -10,6 +10,7 @@ const images = ref([
 ]);
 
 watchEffect(() => {
+  currentImage.value = 'https://picsum.photos/id/77/800/800'
   images.value[0] = 'https://picsum.photos/id/77/800/800'
 })
 
@@ -19,7 +20,7 @@ watchEffect(() => {
   <div id="ItemPage" class="mt-4 max-w-[1200px] mx-auto px-2">
     <div class="md:flex gap-4 justify-between mx-auto w-full">
       <div class="md:w-[40%]">
-        <img :src="`https://picsum.photos/id/95/800/800`" alt="" class="rounded-lg object-cover">
+        <img :src="currentImage" alt="" class="rounded-lg object-cover">
         <div class="flex items-center justify-center mt-2" v-if="images[0]">
           <div v-for="image in images">
             <img
@@ -28,7 +29,6 @@ watchEffect(() => {
                 class="rounded-md object-cover border-[3px] cursor-pointer"
                 width="70"
                 @mouseover="currentImage = image"
-                @mouseleave="currentImage = null"
                 @click="currentImage = image"
                 :class="currentImage === image ? 'border-[#FF5353]' : ''"
             >
