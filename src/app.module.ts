b0e8915c97635b9fa5130caa9f49eb646754ed84
Akin Akinbobola <akinbobola71@gmail.com>
2024-07-04 11:07:45 +0100

    Add users files

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,8 +2,9 @@ import { Module } from '@nestjs/common';
 import { PrismaModule } from './prisma/prisma.module';
 import { AuthModule } from './auth/auth.module';
 import { ProductsModule } from './products/products.module';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [PrismaModule, AuthModule, ProductsModule],
+  imports: [PrismaModule, AuthModule, ProductsModule, UsersModule],
 })
 export class AppModule {}
